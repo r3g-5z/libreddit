b2f305e88a4d7ff6114bc51d85ee868f4a4c4b93
r3g_5z <june@girlboss.ceo>
2023-01-16 01:33:45 -0500

    add X-XSS-Protection 0

--- src/main.rs
+++ src/main.rs
@@ -172,6 +172,7 @@ async fn main() {
 		"Referrer-Policy" => "no-referrer",
 		"X-Content-Type-Options" => "nosniff",
 		"X-Frame-Options" => "DENY",
+		"X-XSS-Protection" => "0",
 		"Content-Security-Policy" => "default-src 'none'; font-src 'self'; script-src 'self' blob:; manifest-src 'self'; media-src 'self' data: blob: about:; style-src 'self' 'unsafe-inline'; base-uri 'none'; img-src 'self' data:; form-action 'self'; frame-ancestors 'none'; connect-src 'self'; worker-src blob:;"
 	};
 
